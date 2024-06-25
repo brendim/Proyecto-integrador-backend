6d04aebd59c96cdeea3a4fba73feb708fb89362a
brendim <brenda.mulato.c@gmail.com>
2024-06-25 16:12:07 -0400

    primer commit

---
+++ src/main/java/com/cuatro/minga_backend/MingaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cuatro.minga_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuatro/minga_backend/MingaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuatro.minga_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
