e0851f49e69a1929688681471ae0dad293f79ad6
Belén <belen.munoz@usach.cl>
2024-06-26 10:52:06 -0400

    refactor: usuario.java a Usuario2.java
    Commit fantasma para poder dejar el archivo en mayúscula.

---
+++ src/main/java/com/cuatro/minga_backend/models/Usuario2.java
@@ -0,0 +1,49 @@
+package com.cuatro.minga_backend.models;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //Crea AllArgsConstructor, Getters, Setters, Equals, HashCode and ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario2 {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //rut unico para cada usuario
+    @Column(unique = true)
+    private String rut;
+    private String nombre;
+    @Column(name="apellido_paterno")
+    private String apellidoPaterno;
+    @Column(name="apellido_materno")
+    private String apellidoMaterno;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Column(name="fecha_nacimiento")
+    private Date fechaNacimiento;
+    private String celular;
+
+    //Relaciones con otras entidades
+
+    /* //Un usuario puede mandar muchos mensajes
+    @OneToMany(mappedBy = "usuario")
+    private List<Mensaje> mensajes;
+
+    //Un usuario es de una comuna
+    @ManyToOne
+    @JoinColumn(name = "comuna_id")
+    private Comuna comuna;
+
+    //Un usuario puede hacer muchas reseñas
+    @OneToMany(mappedBy = "usuario")
+    private List<Resena> resenas; */
+    
+}
