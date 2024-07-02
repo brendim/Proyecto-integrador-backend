cd48817dafbb60fa6cd0ad53a70e59baca0b7242
Catabel8 <c.espinoza.leiva@gmail.com>
2024-07-02 16:19:49 -0400

    comunas/service/controller/repository

--- src/main/java/com/cuatro/minga_backend/controller/ComunaController.java
+++ src/main/java/com/cuatro/minga_backend/controller/ComunaController.java
@@ -14,40 +14,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cuatro.minga_backend.models.Comuna;
-import com.cuatro.minga_backend.repository.ComunaRepository;
+import com.cuatro.minga_backend.service.ComunaService;
 
 @RestController
 @RequestMapping("/comunas")
 public class ComunaController {
 
     @Autowired
-    private ComunaRepository comunaRepository;
+    private ComunaService comunaService;
 
     @GetMapping
     public List<Comuna> getAllComunas() {
-        return comunaRepository.findAll();
+        return comunaService.getAllComunas();
     }
 
     @GetMapping("/{id}")
     public Optional<Comuna> getComunaById(@PathVariable Long id) {
-        return comunaRepository.findById(id);
+        return comunaService.getComunaById(id);
     }
 
     @PostMapping
     public Comuna createComuna(@RequestBody Comuna comuna) {
-        return comunaRepository.save(comuna);
+        return comunaService.createComuna(comuna);
     }
 
     @PutMapping("/{id}")
     public Comuna updateComuna(@PathVariable Long id, @RequestBody Comuna comunaDetails) {
-        Comuna comuna = comunaRepository.findById(id).orElseThrow();
-        comuna.setNombre(comunaDetails.getNombre());
-
-        return comunaRepository.save(comuna);
+        return comunaService.updateComuna(id, comunaDetails);
     }
 
     @DeleteMapping("/{id}")
     public void deleteComuna(@PathVariable Long id) {
-        comunaRepository.deleteById(id);
+        comunaService.deleteComuna(id);
     }
 }
